48a83f4e008fd0c6a17872fc5d416967d3560e95
SEPIDEH HASHEM ZADEH <sepidehhashemzadeh@SEPIDEHs-MacBook-Pro.local>
2016-11-02 15:41:56 -0700

    inser_user file added

--- BruinSociety/includes/connection.php
+++ BruinSociety/includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("127.0.0.1:81","root","","social_network") or die ("Connection was not established");
+$con=mysqli_connect("127.0.0.1","root","","social_network") or die ("Connection was not established");
 ?>
