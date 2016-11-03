2cb8b19558d116fae6ea5eee175a245f451d6da1
yanglingfeng <yanglingfeng@ucla.edu>
2016-11-03 00:27:57 -0700

    Add getSocietyMembers API which returns all members of a society given a societyid

---
+++ BruinSociety/userSociety.php
@@ -0,0 +1,29 @@
+<?php
+    // This php file is responsible for requests that involves handling the relationship between societies and members
+    $con=mysqli_connect("127.0.0.1","root","","social_network") or die ("Connection was not established");
+    /**
+    * This function prints the ids and names of all the members in a society given the society id.
+    * @return void The function returns void and prints the member information of a certain society. 
+    */
+    function getSocietyMembers() {
+        global $con;
+        echo "aaa<br>";
+        if(isset($_POST['getMembers'])) {
+            $id = mysqli_real_escape_string($con,$_POST['s_id']);
+            echo $id;
+            $query = "select user_society.user_id, users.user_name from user_society inner join users on user_society.id = $id AND user_society.user_id = users.user_id";
+            $result = mysqli_query($con,$query);
+            $check = mysqli_num_rows($result);
+            if($check < 1) {
+                echo "No users found!";
+            } else {
+                while($row = mysqli_fetch_assoc($result)) {   
+                    print"<br>";
+                    print "$row[user_id]<br>";
+                    print "$row[user_name]";
+                    print "<br><br>";
+                }
+            }
+        }
+    }
+?>
