fc314bb9f022edd4995d22121951e28cbfe3e8b1
PlatformTwo <alinandrew007@gmail.com>
2016-11-03 08:46:20 -0700

    Updated posts.php

--- BruinSociety/posts.php
+++ BruinSociety/posts.php
@@ -1,29 +1,34 @@
 <!Doctype html>
-<?php include ("functions/functions.php");?>
+<?php 
+include_once "functions/functions.php";
+include_once "PHLibrary.php";
+
+?>
+
 <html>
 	<head>
 	</head>
 	<style>
 	</style>
 	<body>
-		<div class="container">
-			<div id="head_wrap">
-			</div>
-			<div id="content">
-				<form action="" method="post">
-					<h2>Find a Society to display posts from</h2>
-					<input type="text" name="soc_name" placeholder="Enter a the title of a society." require="required" />
+		<h1> Sorting Demo </h1>
+		<p>This page is for component testing the post displaying capabilities.</p>
+		<p>Enter the name of a society below and all posts from that society will be displayed.</p>
+		</p>
+		<form action="" method="post">
+			<h2>Find a Society to display posts from</h2>
+			<input type="text" name="soc_name" placeholder="Enter a the title of a society." require="required" />
 					<button name="search">Search</button>
-				</form>
+		</form>
 
-				<?php 
-				$id = SearchSocieties();
-				PrintSocietyPosts($id);
+		<?php 
+		if(isset($_POST['search']))
+		{
+			PrintSocietyPosts($_POST['soc_name']);
+		}
 
-				?>
+		?>
 
-			</div>
-		</div>
 		<br><br><p>Andrew Lin</p>
 	</body>
 </html>
