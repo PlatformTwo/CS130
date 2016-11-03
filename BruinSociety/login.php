706fc10f6a917418e14416d40b5932d8959abc89
SepidehHashemzadeh <sepidehhz@g.ucla.edu>
2016-11-02 19:18:47 -0700

    Update login.php

--- BruinSociety/login.php
+++ BruinSociety/login.php
@@ -1,13 +1,4 @@
-//main page that connects different components 
-//Author Sepideh Hashemzadeh
-<?php
-session_start();
-include ("functions/functions.php");
-include ("template/header.php");
-include ("template/content.php");
-include ("template/footer.php");
-include("CheckLogin.php");
-?>
+
 
 
 
