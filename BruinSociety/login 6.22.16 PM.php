0714e7eb7fec5d5dbf7bfad794765e85903fe507
SEPIDEH HASHEM ZADEH <sepidehhashemzadeh@s-149-142-31-51.resnet.ucla.edu>
2016-11-02 18:23:27 -0700

    check login

---
+++ BruinSociety/login 6.22.16 PM.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include ("functions/functions.php");
+include ("template/header.php");
+include ("template/content.php");
+include ("template/footer.php");
+include("CheckLogin.php");
+?>
+
+
+
+        
+       
